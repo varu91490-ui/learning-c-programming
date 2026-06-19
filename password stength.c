863fbb3401af59ba0089115a778bd547c0d3d427
Varshini <varu91490@gmail.com>
2026-06-19 20:43:44 +0530

    remove unrelated old project files

--- password stength.c
+++ password stength.c
@@ -1,1 +1,15 @@
-
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int n;
+ char password=[];
+printf("Enter the password");
+for(i=0;i<=n;i++){
+scanf("%s",&password);}
+le=strlen(password);
+le<=5?printf("weak"):printf("strong in only length");
+switch(password){
+case 1
+}
+}
--- rect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$lemgth=5;
-$breadth=6;
-$area =$length*$breadth;
-echo "Area is $area";
-php>
